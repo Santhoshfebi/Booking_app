8ffa6062a94b2cc1629d810b3e922ab34cd8d6e6
31Bharath <marigowda2bharath@gmail.com>
2024-03-26 11:49:18 +0530

    offers route

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,34 +1,24 @@
-import React from 'react'
- import Navbar from '../Components/Navbar'
- import Header from '../Components/Header'
-
-import MovieCarousal from '../Components/MovieCarousal'
-import Offers from './Offers'
-
-import CardCarousal from '../Components/CardCarousal'
-
-import Carousal from '../Components/Carousal'
-
-import Credit from './Credit'
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import Header from '../Components/Header';
+import MovieCarousal from '../Components/MovieCarousal';
+import CardCarousal from '../Components/CardCarousal';
+import Carousal from '../Components/Carousal';
+// import Credit from './Credit';
+// import Offers from './Offers';
 
 const Home = () => {
   return (
     <>
-
-
-    <Navbar />
-    <Header /> 
-
-    <CardCarousal />
-    <MovieCarousal /> 
-
-
-
-    <Carousal />
-     <Offers />
-     <Credit />
+      <Navbar />
+      <Header />
+      <CardCarousal />
+      <MovieCarousal />
+      <Carousal />
+      {/* <Offers /> */}
+      {/* <Credit /> */}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
