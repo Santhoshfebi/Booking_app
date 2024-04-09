29508bcf468b2161c3eedbce86d9f19bb123b0e2
Rutik Abhang <97541752+Rutik5@users.noreply.github.com>
2024-04-09 20:52:23 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import Navbar from "../Components/Navbar";
-import Header from "../Components/Header";
-import Corporates from "./Corporates";
-import MovieCarousal from "../Components/MovieCarousal";
-import CardCarousal from "../Components/CardCarousal";
-import Carousal from "../Components/Carousal";
-import Credit from "./Credit";
-import Offers from "./Offers";
-import LogIn from "./LogIn";
-import SignIn from "./SignIn";
-import GiftCard from "./GiftCard";
-import SideMenu from "./SideMenu";
-import Addmovie from "../Dashboardcont/Addmovie";
-import Dash from "../Dashboardcont/Dash";
-import Userinfo from "../Dashboardcont/Userinfo";
+// import Navbar from "../Components/Navbar";
+// import Header from "../Components/Header";
+// import Corporates from "./Corporates";
+// import MovieCarousal from "../Components/MovieCarousal";
+// import CardCarousal from "../Components/CardCarousal";
+// import Carousal from "../Components/Carousal";
+// import Credit from "./Credit";
+// import Offers from "./Offers";
+// import LogIn from "./LogIn";
+// import SignIn from "./SignIn";
+// import GiftCard from "./GiftCard";
+// import SideMenu from "./SideMenu";
+// import Addmovie from "../Dashboardcont/Addmovie";
+// import Dash from "../Dashboardcont/Dash";
+// import Userinfo from "../Dashboardcont/Userinfo";
 
 const Home = () => {
   return (
